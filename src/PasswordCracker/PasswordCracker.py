ee9e9bd647a419d4e6246d30b51c653542e8a2a5
root <root@AirportOfficial>
2018-10-22 06:19:49 -0400

    Started function for 512 crack

--- src/PasswordCracker/PasswordCracker.py
+++ src/PasswordCracker/PasswordCracker.py
@@ -6,25 +6,31 @@ Created on Oct 21, 2018
 
 import crypt
 
-def test(cryptPass):
+def testCrack(cryptPass):
     salt = cryptPass[0:2]
     dictfile = open('dictionary.txt','r')
     for word in dictfile.readlines():
         word = word.strip('\n')
         cryptWord = crypt.crypt(word, salt)
         if (cryptWord == cryptPass):
-            print('[+] Found Password '+word+'\n')
+            print('[+] Found Password \''+word+'\'\n')
             return
     print('[-] Password not found. \n')
     
+def crackSHA512(cryptPass):
+    print('[*] This is SHA-512 Hash '+cryptPass)
+    
 def main():
-    passFile = open('passwd','r')
+    passFile = open('PasswordShadow','r')
     for line in passFile.readlines():
         if ':' in line:
             user = line.split(':')[0]
             cryptPass = line.split(':')[1].strip(' ')
-            print('[+] Cracking Password For: '+user)
-            test(cryptPass)
+            print('[*] Cracking Password For: '+user)
+            if (cryptPass[0:3] == '$6$' ):
+                crackSHA512(cryptPass)
+            else:
+                testCrack(cryptPass)
 
 if __name__ == '__main__':
     main()
