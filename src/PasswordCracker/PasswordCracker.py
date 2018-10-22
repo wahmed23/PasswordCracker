4269da1dd47fe0214f12c4485e599b01d565ec26
root <root@AirportOfficial>
2018-10-22 07:30:30 -0400

    SHA-512 cracker. Spoiler: Password is password

--- src/PasswordCracker/PasswordCracker.py
+++ src/PasswordCracker/PasswordCracker.py
@@ -6,8 +6,7 @@ Created on Oct 21, 2018
 
 import crypt
 
-def testCrack(cryptPass):
-    salt = cryptPass[0:2]
+def testCrack(cryptPass, salt):
     dictfile = open('dictionary.txt','r')
     for word in dictfile.readlines():
         word = word.strip('\n')
@@ -17,8 +16,17 @@ def testCrack(cryptPass):
             return
     print('[-] Password not found. \n')
     
-def crackSHA512(cryptPass):
+def crackSHA512(cryptPass,salt):
     print('[*] This is SHA-512 Hash '+cryptPass)
+    print('[*] Salt is :'+ salt)
+    dictfile = open('dictionary.txt','r')
+    for word in dictfile.readlines():
+        word = word.strip('\n')
+        cryptWord = crypt.crypt(word, '$6$'+salt)
+        if (cryptWord == cryptPass):
+            print('[+] Found Password \''+word+'\'\n')
+            return
+    print('[-] Password not found. \n')
     
 def main():
     passFile = open('PasswordShadow','r')
@@ -28,9 +36,11 @@ def main():
             cryptPass = line.split(':')[1].strip(' ')
             print('[*] Cracking Password For: '+user)
             if (cryptPass[0:3] == '$6$' ):
-                crackSHA512(cryptPass)
+                salt = cryptPass[3:11]
+                crackSHA512(cryptPass,salt)
             else:
-                testCrack(cryptPass)
+                salt = cryptPass[0:2]
+                testCrack(cryptPass,salt)
 
 if __name__ == '__main__':
     main()
